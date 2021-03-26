5e2d62fcb590ef33f2b1d006dd7601ec348bb2e9
yeshwanthkasala <78816467+yeshwanthkasala@users.noreply.github.com>
2021-03-26 11:12:41 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,18 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-}  
+  
+package com.geekcap.vmturbo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by shaines on 7/24/16.
+ */
+public class ThingTest {
+
+    @Test
+    public void testN() {
+        Thing t = new Thing();
+        t.setN(5);
+        Assert.assertEquals("N should be 5", 5, t.getN());
+    }
+}
